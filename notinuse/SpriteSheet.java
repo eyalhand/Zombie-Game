44e0e4fb4e4970739670be16e5dbe2350fd2ef60
eyalhand <47476302+eyalhand@users.noreply.github.com>
2019-08-06 18:28:51 +0300

    Add files via upload

---
+++ notinuse/SpriteSheet.java
@@ -0,0 +1,17 @@
+package notinuse;
+
+import java.awt.image.BufferedImage;
+
+public class SpriteSheet {
+
+    private BufferedImage sprite;
+
+    public SpriteSheet(BufferedImage image) {
+        this.sprite = image;
+    }
+
+    public BufferedImage grabImage(int col, int row, int width, int height) {
+        BufferedImage img = sprite.getSubimage((row * 25) - 25, (col * 25) - 25, width, height);
+        return img;
+    }
+}
